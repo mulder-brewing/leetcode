b1f0d7a384fe044a0f275d4838bee3d703bdc1b7
unknown <44846781+mulder-brewing@users.noreply.github.com>
2021-12-02 21:07:44 -0500

    more leetcode practice

--- src/com/company/problems/Problems.java
+++ src/com/company/problems/Problems.java
@@ -3,8 +3,8 @@ package com.company.problems;
 import com.company.model.ListNode;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Problems {
 
@@ -33,7 +33,7 @@ public class Problems {
         return null;
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers_bruteForce(ListNode l1, ListNode l2) {
         BigInteger sum = sumListNode(l1).add(sumListNode(l2));
         String[] digits = sum.toString().split("");
         int[] digitsArray = new int[digits.length];
@@ -62,5 +62,136 @@ public class Problems {
         sb.reverse();
         return new BigInteger(sb.toString());
     }
-    
+
+    public static ListNode addTwoNumbers_elementaryMath(ListNode l1, ListNode l2) {
+        ListNode toReturn = null;
+        ListNode currentNode = null;
+
+        int remainder = 0;
+        while (l1 != null || l2 != null) {
+            int l1Num = l1 == null ? 0 : l1.val;
+            int l2Num = l2 == null ? 0 : l2.val;
+
+            int sum = l1Num + l2Num + remainder;
+            remainder = sum / 10;
+
+            ListNode node = new ListNode(sum % 10);
+
+            if (toReturn == null) {
+                toReturn = node;
+            }
+            if (currentNode == null) {
+                currentNode = node;
+            } else {
+                currentNode.next = node;
+                currentNode = node;
+            }
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (remainder > 0) {
+            currentNode.next = new ListNode(remainder);
+        }
+
+        return toReturn;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] chars = s.toCharArray();
+
+        // Use this for uniqueness
+        Set<Character> characters = new HashSet<>();
+
+        int longestSubstringLength = 1;
+        for (int i = 0; i < chars.length - 1; i++) {
+            characters.add(chars[i]);
+            int longestLengthThisIndex = 1;
+            for (int j = i + 1; j < chars.length; j++) {
+                if (characters.add(chars[j])) {
+                    longestLengthThisIndex++;
+                    if (longestLengthThisIndex > longestSubstringLength) {
+                        longestSubstringLength = longestLengthThisIndex;
+                    }
+                } else {
+                    break;
+                }
+            }
+            characters.clear();
+        }
+
+        return longestSubstringLength;
+    }
+
+    public static int lengthOfLongestSubstring_slidingWindow(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Set<Character> chars = new HashSet<>();
+        int startWindow = 0;
+        int endWindow = 0;
+        int maxLength = 0;
+
+        while (endWindow < s.length()) {
+            if (chars.add(s.charAt(endWindow))) {
+                endWindow++;
+                maxLength = Math.max(maxLength, chars.size());
+            } else {
+                chars.remove(s.charAt(startWindow++));
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] mergedAndSorted = IntStream.concat(IntStream.of(nums1), IntStream.of(nums2)).sorted().toArray();
+        int middle = mergedAndSorted.length / 2;
+        if (mergedAndSorted.length % 2 == 0) {
+            return (mergedAndSorted[middle] + mergedAndSorted[middle - 1]) / 2.0;
+        } else {
+            return mergedAndSorted[middle];
+        }
+    }
+
+    public static String longestPalindrome_bruteForce(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+
+        String longestPalindrome = "";
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                if (isPalindrome(sub) && sub.length() > longestPalindrome.length()) {
+                    longestPalindrome = sub;
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+
+    protected static boolean isPalindrome(String s) {
+        LinkedList<Character> stack = new LinkedList<>();
+        LinkedList<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stack.push(c);
+            queue.add(c);
+        }
+
+        return stack.equals(queue);
+    }
+
 }
--- test/com/company/problems/ProblemsTest.java
+++ test/com/company/problems/ProblemsTest.java
@@ -24,27 +24,27 @@ class ProblemsTest {
     }
 
     @Test
-    void test_addTwoNumbers() {
+    void test_addTwoNumbers_bruteForce() {
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
         LinkedList<Integer> expected = new LinkedList<>();
         expected.add(7);
         expected.add(0);
         expected.add(8);
-        Assertions.assertEquals(expected, Problems.addTwoNumbers(l1, l2).toList());
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_bruteForce(l1, l2).toList());
     }
 
     @Test
-    void test_addTwoNumbers_zeroCase() {
+    void test_addTwoNumbers_bruteForce_zeroCase() {
         ListNode l1 = new ListNode(0);
         ListNode l2 = new ListNode(0);
         LinkedList<Integer> expected = new LinkedList<>();
         expected.add(0);
-        Assertions.assertEquals(expected, Problems.addTwoNumbers(l1, l2).toList());
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_bruteForce(l1, l2).toList());
     }
 
     @Test
-    void test_addTwoNumbers_anotherCase() {
+    void test_addTwoNumbers_bruteForce_anotherCase() {
         ListNode l1 = new ListNode(9);
         ListNode l2 = new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
                 new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))))));
@@ -60,7 +60,7 @@ class ProblemsTest {
         expected.add(0);
         expected.add(0);
         expected.add(1);
-        Assertions.assertEquals(expected, Problems.addTwoNumbers(l1, l2).toList());
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_bruteForce(l1, l2).toList());
     }
 
     @Test
@@ -74,5 +74,79 @@ class ProblemsTest {
         ListNode listNode = new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
                 new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))))));
         Assertions.assertEquals(new BigInteger("9999999991"), Problems.sumListNode(listNode));
+    }
+
+    @Test
+    void test_addTwoNumbers_elementaryMath() {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        LinkedList<Integer> expected = new LinkedList<>();
+        expected.add(7);
+        expected.add(0);
+        expected.add(8);
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_elementaryMath(l1, l2).toList());
+    }
+
+    @Test
+    void test_addTwoNumbers_elementaryMath_zeroCase() {
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = new ListNode(0);
+        LinkedList<Integer> expected = new LinkedList<>();
+        expected.add(0);
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_elementaryMath(l1, l2).toList());
+    }
+
+    @Test
+    void test_addTwoNumbers_elementaryMath_anotherCase() {
+        ListNode l1 = new ListNode(9);
+        ListNode l2 = new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
+                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))))));
+        LinkedList<Integer> expected = new LinkedList<>();
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(0);
+        expected.add(1);
+        Assertions.assertEquals(expected, Problems.addTwoNumbers_elementaryMath(l1, l2).toList());
+    }
+
+    @Test
+    void test_lengthOfLongestSubstring() {
+        Assertions.assertEquals(3, Problems.lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    void test_lengthOfLongestSubstring_slidingWindow() {
+        Assertions.assertEquals(3, Problems.lengthOfLongestSubstring_slidingWindow("pwwkew"));
+    }
+
+    @Test
+    void test_findMedianSortedArrays_odd() {
+        Assertions.assertEquals(2, Problems.findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+    }
+
+    @Test
+    void test_findMedianSortedArrays_even() {
+        Assertions.assertEquals(2.5, Problems.findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+    }
+
+    @Test
+    void test_isPalindrome() {
+        Assertions.assertTrue(Problems.isPalindrome("racecar"));
+        Assertions.assertFalse(Problems.isPalindrome("racecars"));
+        Assertions.assertFalse(Problems.isPalindrome("222020221"));
+    }
+
+    @Test
+    void test_longestPalindrome_bruteForce() {
+        Assertions.assertEquals("bab", Problems.longestPalindrome_bruteForce("babad"));
+        Assertions.assertEquals("a", Problems.longestPalindrome_bruteForce("a"));
+        Assertions.assertEquals("bb", Problems.longestPalindrome_bruteForce("bb"));
     }
 }
